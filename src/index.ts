5e5b002abf5f2574523aaf1f7919fa5bcb137a1d
werb210 <todd.w@boreal.financial>
2026-02-20 09:52:27 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,1 @@
-import express from "express";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const app = express();
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.json({ status: "Agent running" });
-});
-
-app.listen(4000, () => {
-  console.log("Agent service running on port 4000");
-});
+import "./server";
