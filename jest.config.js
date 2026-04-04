d7420c4344112aa8da11aaf055283431e140ef20
werb210 <todd.w@boreal.financial>
2026-04-04 13:35:22 -0600

    fix(agent): tsconfig + enforce token + clean test config

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  setupFilesAfterEnv: ["<rootDir>/test/setup.ts"],
-  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
-  modulePathIgnorePatterns: ["<rootDir>/dist/"],
-};
